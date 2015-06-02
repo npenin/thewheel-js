b690ce02dc40d6ffa16ba27dd53acd32e49714e2
Nicolas Penin <nicolas.penin@dragon-angel.Fr>
2015-06-02 17:17:28 +0200

    Update array-utils.js

--- array-utils.js
+++ array-utils.js
@@ -1,8 +1,8 @@
-Array.prototype.any=function(condition){
+Object.defineProperty(Array.prototype, 'any', {enumerable:false, value:function(condition){
   for(var i=0;i<this.length;i++)
   {
     if(condition.call(this[i], i, this[i]))
       return true;
   }
   return false;
-};
+}});
